621bb16e9722f71565135d373a21a04a874438bd
nicolasardila2002 <nicolasardilarosas@gmail.com>
2025-02-19 15:28:39 -0500

    feat(construccionn ::::): :fire: ejercicio clase
    Ejercicio de explicacion de funcion

---
+++ ejer.py
@@ -0,0 +1,9 @@
+def puede :entrar(nombre,edad):
+if edad < 18:
+    print(nombre,"no puede entrar")
+else:
+    print(nombre,"puede entrar")
+
+
+    puede_entrar("camila","quince")
+    puede_entrar("Nicolas",20)
